c23c457f19d7370541ba67723e6995aa0bf60036
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-03-23 10:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var lookup = {
+const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
@@ -8,22 +8,30 @@ var lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedstr){
-	const words = encodedstr.split(" ");
-	let decodedArr = [];
+function rot13(encodedStr){
+  var words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
 
-	for(let i=0; i<words.length; i++){
-		const woed = words[i]
-		let decoded_word = ''
-		for(let j = 0; j<word.length;j++){
-			var char= word.charAt(j)
-			var decoded_char = lookup(char)
-			decoded_word += decoded_char
-		}
-		decodedArr.push(decoded_word)
-	}
-	return decodedArr.join(" ")
+	
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      var char = word.charAt(j);
+      var decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+
+  return decodedArr.join(" ") ;//return decodedArr
 }
-module.exports=rot13;
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+module.exports = rot13;
 
 	
